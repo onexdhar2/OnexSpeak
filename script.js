4178ba376d48b43b02b40f3de2f942ffe5a8e57c
onexdhar2 <onexdhar667@gmail.com>
2026-03-03 22:58:33 +0530

    fifth part

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ const displayLevelWord = (words) => {
     wordContainer.innerHTML = `<div
         class="text-center bg-sky-100 col-span-full rounded-xl py-10 space-y-6 font-bangla"
       >
+      <img class="mx-auto" src="./assets/alert-error.png"/>
         <p class="text-xl font-medium text-gray-400">
           এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।
         </p>
@@ -34,7 +35,7 @@ const displayLevelWord = (words) => {
     // 3 creat elment
     const card = document.createElement("div");
     card.innerHTML = `<div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
-          <h2 class="font-bold text-2xl">${word.word ? word : "শব্দ পাওয়া যায়নি"}</h2>
+          <h2 class="font-bold text-2xl">${word.word ? word.word : "শব্দ পাওয়া যায়নি"}</h2>
           <p class="font-semibold">${word.meaning ? word.meaning : "অর্থ পাওয়া যায়নি"}</p>
           <div class="font-medium text-2xl font-bangla">${word.pronunciation ? word.pronunciation : "Pronounciation পাওয়া  যায়নি"}</div>
           <div class="flex justify-between items-center">
@@ -46,49 +47,7 @@ const displayLevelWord = (words) => {
     wordContainer.append(card);
   });
 };
-// step 2
-// const displayLevelWord = (words) => {
-//   const wordContainer = document.getElementById("word-container");
-//   wordContainer.innerHTML = "";
 
-//   // ✅ Check first
-//   if (!words || words.length === 0) {
-//     wordContainer.innerHTML = `
-//       <div
-//         class="text-center bg-sky-100 col-span-full rounded-xl py-10 space-y-6 font-bangla"
-//       >
-//         <p class="text-xl font-medium text-gray-400">
-//           এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।
-//         </p>
-//         <h2 class="font-bold text-4xl">নেক্সট Lesson এ যান</h2>
-//       </div>
-//     `;
-//     return;
-//   }
-
-//   // ✅ Then loop
-//   words.forEach((word) => {
-//     const card = document.createElement("div");
-
-//     card.innerHTML = `
-//       <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
-//         <h2 class="font-bold text-2xl">${word.word}</h2>
-//         <p class="font-semibold">${word.meaning}</p>
-//         <div class="font-medium text-2xl font-bangla">${word.pronunciation}</div>
-//         <div class="flex justify-between items-center">
-//           <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]">
-//             <i class="fa-solid fa-circle-info"></i>
-//           </button>
-//           <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]">
-//             <i class="fa-solid fa-volume-high"></i>
-//           </button>
-//         </div>
-//       </div>
-//     `;
-
-//     wordContainer.append(card);
-//   });
-// };
 // step 1
 const displayLessons = (lessons) => {
   // 1 get the container
