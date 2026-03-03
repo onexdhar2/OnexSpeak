86fc32e99c8e6c98ba891c17e5bcb87dbac4d02c
onexdhar2 <onexdhar667@gmail.com>
2026-03-03 19:29:55 +0530

    stepThree

--- script.js
+++ script.js
@@ -16,13 +16,21 @@ const displayLevelWord = (words) => {
   console.log(words);
   // 1 get the container
   const wordContainer = document.getElementById("word-container");
-//   wordContainer.innerHTML = "";
+    wordContainer.innerHTML = "";
   // 2 get every element
   words.forEach((word) => {
     // problem^
     // 3 creat elment
     const card = document.createElement("div");
-    card.innerHTML = `<p>cat</p>`;
+    card.innerHTML = `<div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
+          <h2 class="font-bold text-2xl">${word.word}</h2>
+          <p class="font-semibold">${word.meaning}</p>
+          <div class="font-medium text-2xl font-bangla">${word.pronunciation}</div>
+          <div class="flex justify-between items-center">
+            <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
+            <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
+          </div>
+        </div>`;
     // 4 append ele
     wordContainer.append(card);
   });
